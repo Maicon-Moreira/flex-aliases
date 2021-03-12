7ae12bf9a5f43ebb99290b7cd8dbc7831830d4f6
Maicon-Moreira <maiconmoreirapira@gmail.com>
2021-03-12 13:37:21 -0300

    better classes

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ flex_aliases = ''
 a = '{'
 b = '}'
 
-for ai, aia in [['center', 'c'], ['flex-end', 'e'], ['flex-start', 's']]:
-    for jc, jca in [['space-around', 'a'], ['space-between', 'b'], ['space-evenly', 'e'], ['center', 'c']]:
-        for fd, fda in [['column', 'c'], ['column-reverse', 'cr'], ['row', 'r'], ['row-reverse', 'rr']]:
-            flex_aliases += f'.f{aia}{jca}{fda} {a} display: flex ; align-items: {ai} ; justify-content: {jc} ; flex-direction: {fd} {b}'
+for ai, aia in [['', '0'], ['center', 'c'], ['flex-end', 'e'], ['flex-start', 's']]:
+    for jc, jca in [['', '0'], ['space-around', 'a'], ['space-between', 'b'], ['space-evenly', 'e'], ['center', 'c']]:
+        for fd, fda in [['', '0'], ['column', 'c'], ['column-reverse', 'cr'], ['row', 'r'], ['row-reverse', 'rr']]:
+            flex_aliases += f'.f{aia}{jca}{fda} {a} display: flex;'
+
+            if ai:
+                flex_aliases += f'align-items: {ai};'
+
+            if jc:
+                flex_aliases += f'justify-content: {jc};'
+
+            if fd:
+                flex_aliases += f'flex-direction: {fd};'
+
+            flex_aliases += f'{b}'
 
 
 with open('flexAliases.css', 'w') as f:
