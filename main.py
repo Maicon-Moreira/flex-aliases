236ffcbda244c8c31fef15e5e14750763426b554
Maicon-Moreira <maiconmoreirapira@gmail.com>
2021-03-14 12:25:46 -0300

    improved

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ a = '{'
 b = '}'
 
 for ai, aia in [['', '0'], ['center', 'c'], ['flex-end', 'e'], ['flex-start', 's']]:
-    for jc, jca in [['', '0'], ['space-around', 'a'], ['space-between', 'b'], ['space-evenly', 'e'], ['center', 'c']]:
+    for jc, jca in [['', '0'], ['space-around', 'a'], ['space-between', 'b'], ['space-evenly', 'e'], ['center', 'c'],  ['flex-end', 'e'], ['flex-start', 's']]:
         for fd, fda in [['', '0'], ['column', 'c'], ['column-reverse', 'cr'], ['row', 'r'], ['row-reverse', 'rr']]:
             flex_aliases += f'.f{aia}{jca}{fda} {a} display: flex;'
 
